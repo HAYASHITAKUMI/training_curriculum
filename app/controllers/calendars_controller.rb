f3473a450a835ad0b735ea187f9c928c59cea215
HAYASHITAKUMI <86405682+HAYASHITAKUMI@users.noreply.github.com>
2021-07-12 15:51:21 +0900

    issue6_2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -38,12 +38,9 @@ class CalendarsController < ApplicationController
 
       wday_num = Date.today.wday
       if #「wday_numが7以上の場合」という条件式
-        wday_num >= 7
+        wday_num = wday_num / 7
       end
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => (wday_num)}
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-
       @week_days.push(days)
     end
 
